d6abb5d9f84138718a24d265cc37b343a8d9140c
Roman Kudravcev <romankudravcev1@gmail.com>
2023-05-04 22:42:08 +0200

    add properties file

---
+++ properties.h
@@ -0,0 +1,8 @@
+//define pins
+#define light 25
+#define waterLevelSensor 34
+#define moistureSensor 35
+
+// Replace with your network credentials
+const char* ssid = "iPhone von Roman";
+const char* password = "test1234";
